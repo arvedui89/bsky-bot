ad0e78c0ff4979c13e2d5cb49545b7b1d26a88c8
arvedui89 <arvedui89@o2.pl>
2025-05-22 23:14:07 +0200

    Delete src/lib/getPostText.ts

--- src/lib/getPostText.ts
+++
@@ -1,8 +0,0 @@
-import Parser from "rss-parser";
-
-const parser = new Parser();
-
-export default async function getPostText(): Promise<string> {
-  const feed = await parser.parseURL("https://nitter.net/LFC_pl/rss");
-  return feed.items?.[0]?.title ?? "Brak nowych tweetów";
-}
