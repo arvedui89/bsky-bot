4122ef1719f4ff6b4c082b3490cd485faf4f7ae3
arvedui89 <arvedui89@o2.pl>
2025-05-27 08:33:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,18 @@ console.log('BSKY_HANDLE:', process.env.BSKY_HANDLE);
 console.log('BSKY_PASSWORD:', process.env.BSKY_PASSWORD);
 console.log("Starting bot…");
 
-import Bot from "./lib/bot.js";
-import getPostText from "./lib/getPostText.js";
+import Bot from "./lib/bot";
+import getPostText from "./lib/getPostText";
 
-const text = await getPostText();
+(async () => {
+  const text = await getPostText();
 
-if (!text) {
-  console.log("Brak tekstu do opublikowania. Przerywam.");
-  return;
-}
+  if (!text) {
+    console.log("Brak tekstu do opublikowania. Przerywam.");
+    return;
+  }
 
-await Bot.run(() => Promise.resolve(text), { dryRun: false });
+  await Bot.run(() => Promise.resolve(text), { dryRun: false });
+})();
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
