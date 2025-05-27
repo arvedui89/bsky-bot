377c4f1970a71b80ba8ef8f5d17477cf813ce2bf
arvedui89 <arvedui89@o2.pl>
2025-05-27 12:31:00 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
 import dotenv from "dotenv";
 dotenv.config();
-console.log('Loaded ENV:');
-console.log('BSKY_HANDLE:', process.env.BSKY_HANDLE);
-console.log('BSKY_PASSWORD:', process.env.BSKY_PASSWORD);
+console.log("Loaded ENV:");
+console.log("BSKY_HANDLE:", process.env.BSKY_HANDLE);
+console.log("BSKY_PASSWORD:", process.env.BSKY_PASSWORD);
 console.log("Starting bot…");
 
 import Bot from "./lib/bot.js";
@@ -18,6 +18,6 @@ import getPostText from "./lib/getPostText.js";
   }
 
   await Bot.run(() => Promise.resolve(text), { dryRun: false });
-})();
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+  console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+})();
