7d15669c50b6a190cb0015ab883ee94c45a4fc8f
arvedui89 <arvedui89@o2.pl>
2025-06-05 17:16:42 +0200

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -16,7 +16,7 @@ async function getExternalMetadataFromLink(url: string) {
     };
 
     return {
-      text: `${getMeta("og:title") || "Link"}\n\nℹ️ Zobacz więcej w podglądzie linku.`
+      text: `${getMeta("og:title") || "ʟꜰᴄ Nowy artykuł",
       external: {
         uri: url,
         title: getMeta("og:title") || getMeta("twitter:title"),
